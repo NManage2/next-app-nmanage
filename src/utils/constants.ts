050b900c04f009d937fb181cc13628e10f3addbb
Boussada Noureddine <nmanagel@gmail.com>
2026-04-06 13:22:39 +0200

    commit12

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,5 +2,5 @@ export const POST_PER_PAGE = 6;
 //export const DOMAIN = 'http://localhost:3000/';
 export const DOMAIN =
   process.env.NODE_ENV === 'production'
-    ? 'https://vercel.app'
+    ? 'https://next-app-nmanage.vercel.app'
     : 'http://localhost:3000';
