d13d8485aa8cb6703e1914c8f84bbbe093e3191e
Boussada Noureddine <nmanagel@gmail.com>
2026-04-06 11:07:19 +0200

    fix: simplified prisma client

--- src/utils/lib/prisma.ts
+++ src/utils/lib/prisma.ts
@@ -1,14 +1,15 @@
 import { PrismaClient } from '@prisma/client';
-//import { PrismaClient } from '@/generated/prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient | undefined;
+const prismaClientSingleton = () => {
+  return new PrismaClient();
 };
 
-const connectionString = `${process.env.DATABASE_URL}`;
-const adapter = new PrismaPg({ connectionString });
+declare global {
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
+}
 
-export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
+const prisma = globalThis.prisma ?? prismaClientSingleton();
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+export default prisma;
+
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma;
